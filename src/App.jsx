66baa4b05873cbe8563283a16d76a7ede32b3508
kenmillionaire <osobsim@gmail.com>
2023-03-03 14:27:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.scss";
 // react-router-dom is a package that allows you to create
 //navigation in your app.
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 
 function App() {
   return (
