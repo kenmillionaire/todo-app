9d47bf916ca74cb59be09d9856d55b3b69edf0ac
kenmillionaire <osobsim@gmail.com>
2023-03-03 14:23:09 -0400

    Updates on package.json, App.jsx and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.scss";
+// react-router-dom is a package that allows you to create
+//navigation in your app.
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
