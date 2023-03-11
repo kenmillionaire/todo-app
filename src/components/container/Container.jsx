7d84e3b00a2882e10313058404a71cf024326647
kenmillionaire <osobsim@gmail.com>
2023-03-11 04:27:09 -0400

    Adding container component

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -4,3 +4,8 @@ import "./Container.scss";
 export default function Container() {
   return <div className="container">Container</div>;
 }
+
+// export default function Container({ children }) {
+//   return;
+//   <div className="container">{children}</div>;
+// }
