daa8452ccd16271da5150b8a0749395010990526
kenmillionaire <osobsim@gmail.com>
2023-03-11 05:33:58 -0400

    Added item component Todo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from "../components/container/Container";
 import Header from "../components/container/header/Header";
+import AddTodo from "../components/container/add_todo/AddTodo";
 
 export default function Home() {
   return (
     <Container>
       <Header />
+      <AddTodo />
     </Container>
   );
 }
