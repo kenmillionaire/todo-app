02ec5c0704ccac1c3c240bb3e45f20fb9d9ad6cc
kenmillionaire <osobsim@gmail.com>
2023-03-13 15:15:00 -0400

    Added List item Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Container from "../components/container/Container";
 import Header from "../components/container/header/Header";
 import AddTodo from "../components/container/add_todo/AddTodo";
+import ListTodo from "../components/container/list_todo/ListTodo";
 
 export default function Home() {
   return (
     <Container>
       <Header />
       <AddTodo />
+      <ListTodo />
     </Container>
   );
 }
